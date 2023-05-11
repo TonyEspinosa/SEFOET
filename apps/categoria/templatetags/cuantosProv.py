22213fad0b3f7ce98021f949e4b555acb8d5856c
Tony <tony.kch@gmail.com>
2023-05-11 15:56:11 -0600

    cuantosProv quitar

--- apps/categoria/templatetags/cuantosProv.py
+++
@@ -1,9 +0,0 @@
-from django import template
-register = template.library()
-
-from apps.proveedor.models import m_proveedor
-#from apps.categoria.models import m_categoria
-
-@register.tag
-def cuantosProv(item):
-        return m_proveedor.objects.filter(categoria=item).count()
